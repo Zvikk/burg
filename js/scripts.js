5bb5c3690e5fb9821a20bfacc000c00c13f88048
Zvikktor <35464492+Zvikk@users.noreply.github.com>
2018-02-15 20:18:10 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,28 @@
 //overlay-menu
 const menuButton = document.querySelector('.mobile_menu');
-const modalWrap = document.querySelector('.modal_menu')
-
-menuButton.addEventListener('click', function(e){
-    modalWrap.style.display = 'block';
-});
-
+const modalWrap = document.querySelector('.modal_menu');
+const modalLink = document.querySelectorAll('.modal__link-menu');
 const closeButton = document.querySelector('.close__btn');
 
-closeButton.addEventListener('click', function(e){
+menuButton.addEventListener('click', function (e) {
+    modalWrap.style.display = 'block';
+});
+closeButton.addEventListener('click', function (e) {
     modalWrap.style.display = 'none';
 });
 
-
-$(document).ready(function() {
-$('#fullpage').fullpage({
-css3: true,
-scrollingSpeed: 1000,
-menu: "#globalNav",
-});
-});
+for (let i = 0; i < modalLink.length; i++) {
+    modalLink[i].addEventListener("click", function () {
+        modalWrap.style.display = 'none';
+    });
+};
 
 // acco-menu
 var menuName = document.querySelectorAll(".menu-link");
 var menuItem = document.querySelectorAll(".menu_elem");
 
 for (let i = 0; i < menuName.length; i++) {
-    menuName[i].addEventListener("click", function(e) {
+    menuName[i].addEventListener("click", function (e) {
         e.preventDefault();
 
         for (let j = 0; j < menuItem.length; j++) {
@@ -45,7 +41,7 @@ var teamName = document.querySelectorAll(".team__btn");
 var teamItem = document.querySelectorAll(".team_elem");
 
 for (let i = 0; i < teamName.length; i++) {
-    teamName[i].addEventListener("click", function(e) {
+    teamName[i].addEventListener("click", function (e) {
         e.preventDefault();
 
         for (let j = 0; j < teamItem.length; j++) {
@@ -57,3 +53,35 @@ for (let i = 0; i < teamName.length; i++) {
         this.closest('.team_elem').classList.toggle("active");
     });
 };
+
+// reviews popup
+
+const revPopup = document.querySelector('.reviews_popup');
+const revBtn = document.querySelectorAll ('.btn_reviews');
+const revClose = document.querySelector('.close-reviews');
+
+for ( r=0; r<revBtn.length; r++) {
+    revBtn[r].addEventListener('click', function(){
+        revPopup.style.display = 'flex';
+        revClose.addEventListener('click', function(){
+            revPopup.style.display = 'none';
+        });
+    });
+}
+
+// init onepagescroll
+
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        css3: true,
+        scrollingSpeed: 1000,
+        menu: "#globalNav",
+    });
+});
+
+// slick-slider
+
+$(document).ready(function(){
+    $('.slider__list').slick({
+    });
+  });
